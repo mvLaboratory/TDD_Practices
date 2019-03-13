613ad39b36dd230edadf3d6239ba00a302827a51
Lviv\Volodymyr_Mykytiuk <Volodymyr_Mykytiuk@epam.com>
2019-03-13 22:39:36 +0200

    AppSettingsManager code

--- TDD_Practices/Utils/AppSettingsManager.cs
+++ TDD_Practices/Utils/AppSettingsManager.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Configuration;
 
 namespace TDD_Practices.Utils
 {
   public static class AppSettingsManager
   {
-    
+    public static string GetStringOrDefault(string appSettingName, string defaultValue)
+    {
+      string appSetting = ConfigurationManager.AppSettings[appSettingName];
+      return !string.IsNullOrEmpty(appSetting) ? appSetting : defaultValue;
+    }
+
+    public static int GetIntegerOrDefault(string appSettingName, int defaultValue)
+    {
+      int.TryParse(ConfigurationManager.AppSettings[appSettingName], out var result);
+      return result > 0 ? result : defaultValue;
+    }
+
+    public static bool GetBooleanOrDefault(string appSettingName, bool defaultValue)
+    {
+      return bool.TryParse(ConfigurationManager.AppSettings[appSettingName], out var result) ? result : defaultValue;
+    }
   }
 }
