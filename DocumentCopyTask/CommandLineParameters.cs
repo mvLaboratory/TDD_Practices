9939c23f5ca3363f25e91b2f78327242fb236ee4
Lviv\Volodymyr_Mykytiuk <Volodymyr_Mykytiuk@epam.com>
2019-03-28 19:35:28 +0200

    Task draft

---
+++ DocumentCopyTask/CommandLineParameters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using CommandLine;
+using CommandLine.Text;
+
+namespace DocumentCopyTask
+{
+  public class CommandLineParameters
+  {
+    [Option('p', "Project Type", HelpText = "0: All project types; 1: Public Projects; 2: Private Projects; Default: 0; -p 1")]
+    public int ProjectType { get; set; } = 0;
+
+    [HelpOption]
+    public string GetUsage()
+    {
+      return HelpText.AutoBuild(this, current => HelpText.DefaultParsingErrorsHandler(this, current));
+    }
+
+    public int WriteUsage()
+    {
+      Console.Error.WriteLine(GetUsage());
+      return 2;
+    }
+  }
+}
--- DocumentCopyTask/Program.cs
+++ DocumentCopyTask/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace DocumentCopyTask
 {
-  class Program
+  public class Program
   {
     static void Main(string[] args)
     {
